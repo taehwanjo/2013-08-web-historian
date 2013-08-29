fae5d37ee81e6067919ab0e88f6a9d20930213b2
Hack Reactor Students <github@hackreactor.com>
2013-08-28 19:35:12 -0700

    everything working including cron job

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,17 @@
-// eventually, you'll have some code here that uses the tested helpers 
+// eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
+var x = function(){
+  var filePath = '/Users/hackreactor/Desktop/2013-08-web-historian/data/sites.txt';
+  var htmlFetcherHelpers = require('./lib/html-fetcher-helpers.js');
+
+
+
+  var log = htmlFetcherHelpers.downloadUrls(htmlFetcherHelpers.readUrls(filePath, function(urls){
+    return urls; //urls is definitely an array;
+  }));
+
+  //console.log('typeof log: ' + typeof log);//log.join('\n');
+  return log.join('\n');
+};
+
+x();
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,38 @@
-exports.readUrls = function(filePath, cb){
+var fs = require("fs");
+var httpget = require('http-get');
 
+exports.readUrls = function(filePath, cb){
+  console.log('!filePath: ' + filePath);
+
+  var dataString = fs.readFileSync(filePath, 'utf8');
+  console.log('dataString: ' + dataString);
+
+  var dataArray = dataString.split('\n');
+  console.log('dataArray[0]: ' + dataArray[0]);
+  return cb(dataArray);
 };
 
 exports.downloadUrls = function(urls){
+  if (urls === undefined) return 1;
   // fixme
+  var newFilePath = '/Users/hackreactor/Desktop/2013-08-web-historian/data/sites/';
+  var logArray = [];
+
+var cb = function(error, result){
+      if (error) {
+        console.error("Error while writing: "+error);
+        logArray.push("Error while writing: " + error);
+      } else {
+        console.log('File was downloaded at: ' + result.file);
+        logArray.push('File was downloaded at: ' + result.file);
+      }
+    };
+
+  for(var i=0; i<urls.length; i++) {
+    httpget.get('http://' + urls[i], newFilePath + urls[i], cb); //what in tarnations?
+  }
+
+  return logArray;
+
+
 };
